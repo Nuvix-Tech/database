d6df43c05b9ec84d4a2c1757472699f7d94dd9c4
Ravikant Saini <ravikantsaini047@gmail.com>
2025-01-21 12:55:50 -0500

    feat: add Index decorator and IndexOptions interface for enhanced indexing capabilities

---
+++ src/decorator/Index.ts
@@ -0,0 +1,13 @@
+import { Document } from "../core/Document";
+import { IndexOptions } from "./options/IndexOptions";
+
+
+export function Index(options?: IndexOptions): ClassDecorator {
+  return function (target: any) {
+    const indexes = Reflect.getMetadata('indexes', (target as Object).constructor) || [];
+
+    indexes.push(new Document(options));
+
+    Reflect.defineMetadata('indexes', indexes, target.constructor);
+  };
+}
---
+++ src/decorator/options/IndexOptions.ts
@@ -0,0 +1,12 @@
+
+export interface IndexOptions {
+  $id: string;
+
+  type: 'fulltext' | 'unique' | 'spatial' | 'key';
+
+  attributes: string[];
+
+  lengths?: number[] | any[];
+
+  orders?: ('ASC' | 'DESC')[];
+}
